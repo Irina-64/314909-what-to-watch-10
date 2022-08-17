42a439432ce606015e1c4d265f3855baaff83dcc
Irina Trubchik <trubchik@mail.ru>
2022-08-17 16:43:21 +0300

    выполнен рефакторинг проекта

--- project/src/components/genres/genre-list/genre-list.tsx
+++ project/src/components/genres/genre-list/genre-list.tsx
@@ -1,12 +1,32 @@
-import TList from '../../../types/list';
+import { useCallback, useEffect } from 'react';
+import { Genre } from '../../../const/enums';
+import useAppDispatch from '../../../hooks/use-app-dispatch/use-app-dispatch';
+import useAppSelector from '../../../hooks/use-app-selector/use-app-selector';
+import { changeGenre, resetGenre } from '../../../store/main-page-actions';
+import { getMovies, getSelectedGenre } from '../../../utilites/selectors/selectors';
 import GenreItem from '../genre-item/genre-item';
 
-const GenreListComponent = ({ genres }: TList<string>) => {
-  const genreItems = genres.map((genre) => <GenreItem key={genre} value={genre} />);
+const GenreListComponent = () => {
+  const movies = useAppSelector(getMovies);
+  const selectedGenre = useAppSelector(getSelectedGenre);
+  const dispatch = useAppDispatch();
+
+  const currentGenres = [Genre.AllGenres, ...new Set(movies.map((movie) => movie.genre as Genre))];
+
+  const handleGenreClick = useCallback(
+    (genre: Genre) => {
+      dispatch(changeGenre(genre));
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    dispatch(resetGenre());
+  }, [dispatch]);
 
   return (
     <ul className="catalog__genres-list">
-      {genreItems}
+      {currentGenres.map((genre) => <GenreItem key={genre} genre={genre} selectedGenre={selectedGenre} handleGenreClick={handleGenreClick} />)}
     </ul>
   );
 };
